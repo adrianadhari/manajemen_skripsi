637e274df2c0a732eb6d34358ceafcef177fa5d2
adrianadhari <adrianadhari@protonmail.com>
2025-05-06 21:51:38 +0700

    logout

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,12 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('guest')->group(function () {
+
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+});
+
+Route::middleware('auth')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+});
