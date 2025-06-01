52fbe426e04b791b01a40ca9aef592978e1b907e
adrianadhari <adrianadhari@protonmail.com>
2025-06-01 22:22:53 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('index');
+});
+
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 
